aa9a5c7bc6899c1cf88223329f5f0312c35192f9
Ecaterina Pihurov <ecaterinaoprea92@gmail.com>
2021-04-30 11:49:49 +0300

    Second commit

--- tip.js
+++ tip.js
@@ -1,14 +1,14 @@
 let calculate = document.getElementById("calculate");
-let ServiceLevel = document.querySelector("div");
+let ServiceLevel = document.getElementsByName("selection");
 let NumberOfPersons = document.getElementById("NumberofPersons");
-let tipValue = document.getElementsByClassName("tip");
+let tipValue = document.getElementById("tip");
 
-calculate.addEventListener('click', () => {
+calculate.addEventListener("click", () => {
     let total = document.getElementById("Totalbill").value;
-    for (let i =0; i < ServiceLevel.length; i++) {
+    for (let i = 0; i < ServiceLevel.length; i++) {
         if(ServiceLevel[i].checked) {
-            tipValue.innerHTML = total * ServiceLevel[i].value / NumberOfPersons.value;
+            tipValue.innerHTML = (total * ServiceLevel[i].value) / NumberOfPersons.value;
         }
     }
      
-})
+});
